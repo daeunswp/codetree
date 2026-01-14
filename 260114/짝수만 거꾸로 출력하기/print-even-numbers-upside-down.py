c153ae81d2245eedfc01acc47bbd296cba9b520a
daeunswp <daeunswp@gmail.com>
2026-01-14 23:35:18 +0900

    perf: 최적화된 참고코드 주석 추가

--- 260114/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 260114/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -9,4 +9,12 @@ for i in range(n):
 for i in range(len(even_list)-1, -1, -1):
     print(even_list[i], end=" ")
 
+# 최적화 코드
+"""
+n = int(input())
+arr = list(map(int, input().split()))
 
+for i in range(n - 1, -1, -1):
+    if arr[i] % 2 == 0:
+        print(arr[i], end=" ")
+"""
