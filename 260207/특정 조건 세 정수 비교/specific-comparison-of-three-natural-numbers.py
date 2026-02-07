ce2ea35f413580b090783350b4f53be9ff2e1cc7
daeunswp <121616505+daeunswp@users.noreply.github.com>
2026-02-07 21:01:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260207/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 260207/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -2,8 +2,20 @@ a, b, c = map(int, input().split())
 
 first = 0
 second = 0
+least = 0
 
-if a<=b<=c:
+if a<=b:
+    if a<=c:
+        least = a
+    else:
+        least = c
+else:
+    if b<=c:
+        least = b
+    else:
+        least = c
+
+if least == a:
     first = 1
 
 if a==b==c:
